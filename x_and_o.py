f29959c9151fe9609afbd73d5005dab3f500b4b3
madrow1 <rowan.pitt@btinternet.com>
2023-01-27 16:15:33 +0000

    jty

--- x_and_o.py
+++ x_and_o.py
@@ -4,8 +4,6 @@ def count_x_o(input):
     count_x = 0
     count_o = 0
     for letter in range(len(input)):
-        print(letter)
-
         if input[letter].lower() == "x":
             count_x += 1
         if input[letter].lower() == "o":
@@ -17,4 +15,4 @@ def count_x_o(input):
     else:
         return True
 
-print(count_x_o("xxoxoxooo"))
+print(count_x_o("xxoxoxoxoo"))
